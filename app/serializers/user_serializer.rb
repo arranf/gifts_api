f7df3ad5bd25d4e32650fe7f5f5600a1875240fe
Arran France <arranf@gmail.com>
2016-12-28 17:37:17 +0000

    Prevent user serialization from providing gifts data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :lists
 
   has_many :lists
+  class ListSerializer < ActiveModel::Serializer
+    attributes :id, :name, :is_private, :is_default
+  end
 end
