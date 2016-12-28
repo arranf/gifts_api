5774759d6436b8d320fcef96615c0df33e72f8b5
Arran France <arranf@gmail.com>
2016-12-28 16:47:38 +0000

    Prevent recursive serialization when querying a list

--- app/serializers/gift_serializer.rb
+++ app/serializers/gift_serializer.rb
@@ -1,5 +1,5 @@
 class GiftSerializer < ActiveModel::Serializer
-  attributes :id, :name, :cost, :list, :purchased
+  attributes :id, :name, :cost, :purchased
 
   belongs_to :list_id
 end
--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -2,5 +2,4 @@ class ListSerializer < ActiveModel::Serializer
   attributes :id, :name, :is_private, :is_default, :gifts
 
   has_many :gifts
-  has_one :user
 end
