6ba01da075cdebbff0ed7ed7be340111dedb1bf8
Arran France <arranf@gmail.com>
2016-12-28 17:38:00 +0000

    Change user controller to take the user's base64 urlsafe encoded email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      email = Base64.urlsafe_decode64(params[:id])
+      @user = User.find_by_email(email)
     end
 
     # Only allow a trusted parameter "white list" through.
